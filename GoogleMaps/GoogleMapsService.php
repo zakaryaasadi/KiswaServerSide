00dc48c16fa57157c7656d878159f87bc3450c9c
Zakarya Asadi <zakaryaasadi@gmail.com>
2022-03-16 13:04:23 +0400

    Update google map service

--- GoogleMaps/GoogleMapsService.php
+++ GoogleMaps/GoogleMapsService.php
@@ -3,21 +3,27 @@
 namespace GoogleMaps;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class GoogleMapsService{
     public function GetLocationByAddress( $address){
         $response = $this->FetchData($address);
-        if($response->ok()){
-            if(!isset($response->json()['results'])){
-                $results = $response->json()["results"];
-                if(Count($results) > 0){
-                    $location = $results[0]["geometry"]["location"];
-                    return new GoogleMapsLocationModel($location["lat"], $location["lng"]);
+        try{
+            if($response->ok()){
+                    $results = $response->json()["results"];
+                    if(Count($results) > 0){
+                        $location = $results[0]["geometry"]["location"];
+                        return new GoogleMapsLocationModel($location["lat"], $location["lng"]);
                 }  
-            }  
-        }
+            }
 
-        return new GoogleMapsLocationModel(0 , 0);
+            return new GoogleMapsLocationModel(0 , 0);
+        }catch(Throwable $e){
+            Log::channel("api_log")->error($response->json());
+            Log::channel("api_log")->error($e->getMessage());
+            return new GoogleMapsLocationModel(0 , 0);
+        }
     }
 
     private function FetchData($address){
